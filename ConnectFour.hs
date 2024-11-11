eb6ceeb4e5752e950b4601a53838e2b0b6dbf333
L-Perry-Laptop <lperry@trinity.edu>
2024-11-11 16:32:51 -0600

    Winstate tests written, untested

--- ConnectFour.hs
+++ ConnectFour.hs
@@ -28,10 +28,10 @@ winState newMove tokens = checkDirs newMove dirs
                else adjacent ((r1 c1),pl1) ts
          dirs = adjacent newMove tokens -- this is the list of adjacent tokens
 				 checkDirs _ [] = False
-         checkDirs ((r1 c2), pl) ((r2 c2):ds) = if (checkFour (r1 c1) (r2 c2)) then True else (checkDirs ((r1 c2), pl) ds)
+         checkDirs ((r1 c2), pl) ((r2 c2):ds) = if (checkFour ((r1 c1), pl) (r2 c2)) then True else (checkDirs ((r1 c2), pl) ds)
 				 -- ^ checks each direction, returning true if any direction creates a connect four
-         checkFour (r1 c1) (r2 c2) = if (rdif==4 || cdif==4) then True else
-                                    (if (lookFor (a b) tokens) then (checkFour (r1 c1) (a b)) else False) -- looks for connect four, if looking for a fifth then win
+         checkFour ((r1 c1), pl) (r2 c2) = if (rdif==4 || cdif==4) then True else
+                                    (if (lookFor ((a b), pl) tokens) then (checkFour ((r1 c1), pl) (a b)) else False) -- looks for connect four, if looking for a fifth then win
             where rdif = r2 - r1 --row difference
                   cdif = c2 - c1 --column difference
                   a = r1+(newD rdif) --row value of next token in connect four
@@ -40,7 +40,18 @@ winState newMove tokens = checkDirs newMove dirs
 
 lookFor :: Coordinate -> [Tokens] -> Boolean
 lookFor _ [] = False --Checks if token with certain cordinates exists in a list
-lookFor (r1 c1) (((r2 c2),pl):ts) = if ((r1==r2)&&(c1==c2)) then True else checkFor (r1 c1) ts
+lookFor ((r1 c1), pl1) (((r2 c2),p2):ts) = if ((r1==r2) && (c1==c2) && (pl1==pl2)) then True else lookFor ((r1 c1), pl1) ts
+
+
+--story 2 testing
+list1 = [((1 2), Red), ((1 3), Red), ((1 4), Red)] --returns true with input1, false with input2
+input1 = ((1 5), Red)
+input2 = ((2 5), Red)
+list2 = [((1 2), Red), ((2 2), Red), ((3 2), Red)] --returns true with input 3
+input3 = ((4 2), Red)
+list3 = [((1 2), Red), ((1 3), Black), ((1 4), Red)] --returns false with input1
+list4 = [((1 1), Red), ((2 2), Black), ((3 3), Red)] --returns true with input4
+input4 = ((4 4), Red)
 
 -- STORY 3
 -- move (Player, Column) = //check for other tokens in column, row is next up, check if legalMove
