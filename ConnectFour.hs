a775de865534e953d44f1b21784a1320e3a66432
William Wallace <wwallac1@trinity.edu>
2024-11-12 17:32:15 -0600

    Simplified Move type and made Player derive Eq and Show

--- ConnectFour.hs
+++ ConnectFour.hs
@@ -1,14 +1,14 @@
-data Player = Red | Black
+import Data.Maybe
 
--- Do something to limit row/column to 6/7?
-type Coordinate = (Row, Column)
-type Row = Int 
-type Column = Int 
+data Player = Red | Black deriving (Eq, Show)
+type Coordinate = (Row, Column) 
+type Row = Int
+type Column = Int
 
 type Token = (Coordinate, Player)
 type Grid = [Token]
 type Game = (Grid, Player) -- Current grid and whose turn it is
-type Move = (Player, Column)
+type Move = Column
 
 -- STORY 2
 -- winState (t:ts) = //run through each token
