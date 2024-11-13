1118a7f8cb31a9fa959bd5434f78848f206fea99
huang-el <ehuang1@trinity.edu>
2024-11-13 12:08:00 -0600

    story3

--- ConnectFour.hs
+++ ConnectFour.hs
@@ -20,12 +20,33 @@ winState = undefined
 
 -- STORY 3
 -- move (Player, Column) = //check for other tokens in column, row is next up, check if legalMove
-move :: Game -> Move -> Game
-move = undefined
+
+nextPlayer :: Player -> Player
+nextPlayer Red = Black
+nextPlayer Black = Red
+
+nextAvailableRow :: Grid -> Column -> Maybe Row
+nextAvailableRow grid col = undefined
+--   let allRows = [0..5]
+--       occupiedRows = [row | ((row c), _) <- grid, c == col]
+--       availableRows = filter (`notElem` occupiedRows) allRows
+--   in if null availableRows then Nothing else Just (head availableRows)
+
+updateBoard :: Game -> Move -> Game
+updateBoard (grid, currentPlayer) (player, col) = undefined
+--   case nextAvailableRow grid col of
+--     Nothing -> (grid, currentPlayer)
+--     Just row -> 
+--       let newToken = ((row, col), player)
+--           newGrid = newToken : grid
+--       in (newGrid, nextPlayer player)
+
+makeMove :: Game -> Move -> Game
+makeMove game move = if move `elem` legalMoves game then updateBoard game move else game
 
 --STORY 4
 -- (Within 6 rows and 7 columns (possible variable numbers))
-legalMoves :: Grid -> [Move]
+legalMoves :: Game -> [Move]
 legalMoves = undefined
 
 -- STORY 5
