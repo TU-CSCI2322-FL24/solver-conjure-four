0da443a6a4fd0e1bcf71353610f36897a6b230de
Jaden Kai Rangel <jrangel1@trinity.edu>
2024-11-21 22:01:54 -0600

    story 11,12,13

--- ConnectFour.hs
+++ ConnectFour.hs
@@ -1,35 +1,72 @@
-import Prettyprinter
-import Prettyprinter.Render.Text (putDoc)
-import Data.Maybe (fromMaybe)
+import Data.Maybe
 
-data Player = Red | Black
-data Coordinate = Coordinate Int Int deriving (Eq, Show)
--- Do something to limit row/column to 6/7?
-type Row = Int 
-type Column = Int 
+data Player = Red | Black deriving (Eq, Show)
+type Coordinate = (Row, Column) 
+
+type Row = Int
+type Column = Int
 
 type Token = (Coordinate, Player)
 type Grid = [Token]
 type Game = (Grid, Player) -- Current grid and whose turn it is
-type Move = (Player, Column)
+type Move = Column
+
+data Win = Winner Player | Ongoing | Tie deriving (Show, Eq)
 
 -- STORY 2
--- winState (t:ts) = //run through each token
--- Maybe a function for each direction instead?
---    where direction = //list of directions of adjacent tokens
---               Four = //check in direction for four tokens
-winState :: Game -> Maybe Player
-winState = undefined
+
+winState :: Token -> Grid -> Win
+winState ((r,c),pl) grid = 
+   let dirs = [[(r+i,c) | i <- [-3..3]], [(r,c+i) | i <- [-3..3]], [(r+i,c+i) | i <- [-3..3]], [(r+i,c-i) | i <- [-3..3]]] --list of list of coordinates
+       checkGrid = map (map (\coord -> lookup coord grid)) dirs --list of list of maybe players
+       checkWin = map checkFour checkGrid --list of Maybe Players
+   in if ((Just pl) `elem` checkWin) then Winner pl else Ongoing --later check for tie
+
+checkFour :: [Maybe Player] -> Maybe Player
+checkFour [] = Nothing
+checkFour (Just Red:Just Red:Just Red:Just Red:rest) = Just Red
+checkFour (Just Black:Just Black:Just Black:Just Black:rest) = Just Black
+checkFour (x:rest) = checkFour rest
+
+--story 2 testing
+list1, list2, list3, list4 :: Grid
+input1, input2, input3, input4 :: Token
+list1 = [((1, 2), Red), ((1, 3), Red), ((1, 4), Red), ((1, 5), Red), ((2, 5), Red)]
+input1 = ((1, 5), Red)
+input2 = ((2, 5), Red)
+list2 = [((1, 2), Red), ((2, 2), Red), ((3, 2), Red), ((4, 2), Red)] --returns Red with input 3
+input3 = ((4, 2), Red)
+list3 = [((1, 2), Red), ((1, 3), Black), ((1, 4), Red), ((1, 5), Red)] --returns Ongoing with input1
+list4 = [((1, 1), Red), ((2, 2), Red), ((3, 3), Red), ((4, 4), Red)] --returns Red with input4
+input4 = ((4, 4), Red)
 
 -- STORY 3
--- move (Player, Column) = //check for other tokens in column, row is next up, check if legalMove
-move :: Game -> Move -> Game
-move = undefined
+nextPlayer :: Player -> Player
+nextPlayer Red = Black
+nextPlayer Black = Red
+
+updateBoard :: Game -> Move -> Game
+updateBoard (grid, currentPlayer) col = 
+    let aux grid col row = 
+            case lookup (row, col) grid of
+                Just player -> aux grid col (row+1)
+                Nothing -> 
+                    let newToken = ((row, col), currentPlayer)
+                        newGrid = newToken : grid
+                    in (newGrid, nextPlayer currentPlayer)
+    in aux grid col 1
+
+makeMove :: Game -> Move -> Game
+makeMove game move = if move `elem` legalMoves game then updateBoard game move else game
 
 --STORY 4
--- (Within 6 rows and 7 columns (possible variable numbers))
-legalMoves :: Grid -> [Move]
-legalMoves = undefined
+-- takes a grid and a column number and returns
+-- true if that column is not full of tokens
+isNotEmpty :: Grid -> Int -> Bool
+isNotEmpty grid column = isNothing $ lookup (6, column) grid
+
+legalMoves :: Game -> [Move]
+legalMoves (grid, player) = [col | col <- [1..7], isNotEmpty grid col]
 
 -- STORY 5
 
@@ -40,11 +77,107 @@ prettyPrint grid = unlines [ prettyRow r | r <- reverse [1..6]]
     prettyRow r = unwords [ prettyCell r c | c <- [1..7]]
     
     prettyCell :: Row -> Column -> String
-    prettyCell r c = case lookup (Coordinate r c) grid of
+    prettyCell r c = case lookup (r, c) grid of
       Just Red   -> "o"
       Just Black -> "x"
       Nothing    -> "."
 
+
+
+
+-- SPRINT 2
+
+-- STORY 9
+-- Considers every valid move, the resulting game state, and chooses the move with the 
+-- best outcome for the current player. This will involve recursively searching through 
+-- the game states that result from that move. Think Scavenge!
+whoWillWin :: Game -> Win
+whoWillWin = undefined
+
+
+-- STORY 10
+-- Given a game state, you should  return a move that can force a win for the current 
+-- player. Failing that, return a move that can force a tie for the current player.
+-- This is very similar to whoWillWin, but keeps track of what the first move was that 
+-- could force that outcome. That means you should not use this function to write whoWillWin.
+bestMove :: Game -> Move
+bestMove = undefined
+
 -- STORY 11
+-- Given a game state, you should  return a move that can force a win for the current player.
+-- Failing that, return a move that can force a tie for the current player.
+-- This is very similar to whoWillWin, but keeps track of what the first move was that could force that outcome.
+-- That means you should not use this function to write whoWillWin
+showGame :: Game -> String
+showGame (grid, currentPlayer) =
+    let tokens = unlines $ map showToken grid
+        turn = "Turn:" ++ showPlayer currentPlayer
+    in tokens ++ turn
+  where
+    showToken :: Token -> String
+    showToken ((r, c), player) = show r ++ "," ++ show c ++ ":" ++ showPlayer player
+
+    showPlayer :: Player -> String
+    showPlayer Red = "R"
+    showPlayer Black = "B"
+
 -- STORY 12
+-- Takes a string in your text format and returns the corresponding game
+readGame :: String -> Game
+readGame str =
+    let lines' = lines str
+        tokens = mapMaybe parseToken (init lines')
+        currentPlayer = parsePlayer (drop 5 (last lines'))
+    in (tokens, currentPlayer)
+  where
+    parseToken :: String -> Maybe Token
+    parseToken line =
+      case span (/= ':') line of
+        (coords, ':':player) ->
+          let (r, ',':c) = span (/= ',') coords
+          in Just ((read r, read c), parsePlayer player)
+        _ -> Nothing
+
+    parsePlayer :: String -> Player
+    parsePlayer "R" = Red
+    parsePlayer "B" = Black
+    parsePlayer _ = error "Invalid player symbol"
+
+
 -- STORY 13
+-- Takes a game and turns it into a string in your text format
+printGame :: Game -> String
+printGame (grid, currentPlayer) =
+    let tokens = unlines $ map showToken grid
+        turn = "Turn:" ++ showPlayer currentPlayer
+    in tokens ++ turn
+  where
+    showToken :: Token -> String
+    showToken ((r, c), player) = show r ++ "," ++ show c ++ ":" ++ showPlayer player
+
+    showPlayer :: Player -> String
+    showPlayer Red = "R"
+    showPlayer Black = "B"
+
+
+-- STORY 14
+-- Define a series of IO actions to wrap around these functions, as well as bestMove, and 
+-- define a main IO action. You will need to build four I/O actions: one each to read and 
+-- write game states from a file, one that computes and prints the winning move, and a simple main action.
+
+-- At least some of these actions will need to be in a Main module that imports your other module(s).
+writeGame :: Game -> FilePath -> IO ()
+writeGame = undefined
+
+loadGame :: FilePath -> IO Game
+loadGame = undefined
+
+-- Computes the best move and prints it to standard output. 
+-- For full credit, also print the outcome that moves forces.
+putBestMove :: Game -> IO ()
+putBestMove = undefined
+
+-- Reads a file name from standard input or the arguments, 
+-- loads the game, and prints the best move
+main :: IO ()
+main = undefined
