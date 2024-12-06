a0b5d568970d1d4e7d2aec046a54d64ad0c3fe50
William Wallace <wwallac1@trinity.edu>
2024-12-06 12:12:12 -0600

    Create Story 15 Test Files

--- TestGrids.hs
+++ TestGrids.hs
@@ -7,12 +7,14 @@ emptyGrid, fullColGrid, fullGrid, oneMoveLeft, twoMovesLeft, fourMovesLeft :: Gr
 emptyGrid = []
 fullColGrid = [((1, 1), Red), ((2, 1), Black), ((3, 1), Red), ((4, 1), Black), ((5, 1), Red), ((6, 1), Black)]
 fullGrid = [((row, col), Red) | row <- [1..6], col <- [1..7]]
-oneMoveLeft = init fullGrid -- Every space filled but (6, 7) (top right)
+
+-- Whoever goes first wins
+oneMoveLeft = (((6, 5), Red)):twoMovesLeft
 
 -- Up to two moves left in the game.
 -- Red wins with either move if they play first.
 -- If Black moves first, they win on column 4 and lose on column 5.
-twoMovesLeft = fourMovesLeft ++ [((5, 4), Red), ((5, 5), Black)]
+twoMovesLeft = [((5, 4), Red), ((5, 5), Black)] ++ fourMovesLeft
 
 -- Up to four moves left in the game. 
 -- Red forces a win if they play column 4 first. 
